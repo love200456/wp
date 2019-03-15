4496eeeaaa72c0a7ee5635b3821d8788ec00e45c
love200456 <71337642@qq.com>
2019-03-15 14:44:37 +0800

    要添加注释

--- progit/src/com/pro/MyTest.java
+++ progit/src/com/pro/MyTest.java
@@ -1,10 +1,27 @@
 package com.pro;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class MyTest {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-
+		String str="dddÄãºÃ¿ÉÒÔÂðjjdk";
+		InputStream is = null;
+		byte[] bs=new byte[1024];
+		int i=0;
+		try{
+			is=new ByteArrayInputStream(str.getBytes());
+			int n=-1;
+			while((n=is.read())!=-1){
+				bs[i++]=(byte)n;
+			}
+			String s=new String(bs,"gbk");
+			System.out.println("s="+s);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
